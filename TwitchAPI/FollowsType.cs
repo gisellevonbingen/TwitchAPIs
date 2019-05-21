a1bbe170801cdff4a4412a7c7bebf0ad768b90da
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-05-21 14:26:48 +0900

    From, To 주석 추가
    From : XXX followed from id, Followings
    To : XXX follow to id, Followers

--- TwitchAPI/FollowsType.cs
+++ TwitchAPI/FollowsType.cs
@@ -8,7 +8,13 @@ namespace TwitchAPI
 {
     public class FollowsType
     {
+        /// <summary>
+        /// XXX followed from id, Followings
+        /// </summary>
         public static FollowsType From { get; } = new FollowsType("from", "to");
+        /// <summary>
+        /// XXX follow to id, Followers
+        /// </summary>
         public static FollowsType To { get; } = new FollowsType("to", "from");
 
         public string Request { get; }
