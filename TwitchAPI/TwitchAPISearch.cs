4c1107e02f9588e08ce7921d7946e676b7b96ac1
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-05-23 15:58:08 +0900

    TwitchChannel Parse

--- TwitchAPI/TwitchAPISearch.cs
+++ TwitchAPI/TwitchAPISearch.cs
@@ -43,36 +43,11 @@ namespace TwitchAPI
             for (int i = 0; i < values.Length; i++)
             {
                 var itemToken = arrayToken[i];
-                values[i] = this.ParseChannel(itemToken);
+                var value = values[i] = new TwitchChannel();
+                value.Read(itemToken);
             }
 
             return values;
-        }
-
-        public TwitchChannel ParseChannel(JToken token)
-        {
-            var value = new TwitchChannel();
-            value.Id = token.Value<string>("_id");
-            value.BroadcasterLanguage = token.Value<string>("broadcaster_language");
-            value.CreatedAt = token.Value<DateTime>("created_at");
-            value.DisplayName = token.Value<string>("display_name");
-            value.Followers = token.Value<int>("followers");
-            value.Game = token.Value<string>("game");
-            value.Language = token.Value<string>("language");
-            value.Logo = token.Value<string>("logo");
-            value.Mature = token.Value<bool>("mature");
-            value.Name = token.Value<string>("name");
-            value.Partner = token.Value<bool>("partner");
-            value.ProfileBanner = token.Value<string>("profile_banner");
-            value.ProfileBannerBackgroundColor = token.Value<string>("profile_banner_background_color");
-            value.Status = token.Value<string>("status");
-            value.UpdatedAt = token.Value<DateTime>("updated_at");
-            value.Url = token.Value<string>("url");
-            value.VideoBanner = token.Value<string>("video_banner");
-            value.Views = token.Value<int>("views");
-
-
-            return value;
         }
 
     }
