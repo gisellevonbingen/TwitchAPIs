e82d41f7081aa10b2d4d7e3832dcd67bb61f98be
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-05-27 19:56:00 +0900

    Update TwitchAPIHandler.cs

--- Test/TwitchAPIHandler.cs
+++ Test/TwitchAPIHandler.cs
@@ -16,12 +16,7 @@ namespace TwitchAPI.Test
 
         public TwitchAPI API { get; private set; }
         public OAuthRequest OAuthRequest { get; private set; }
-        public string OAuthURI { get; private set; }
-        public Uri ResponseURI { get; private set; }
         public OAuthAuthorization OAuthAuthorization { get; set; }
-
-        private Form Form;
-        private WebBrowser Browser;
 
         public TwitchAPIHandler(UserAbstract user)
         {
@@ -34,32 +29,26 @@ namespace TwitchAPI.Test
             var clientId = user.ReadInput("Enter Client-ID");
             var api = this.API = new TwitchAPI();
             api.ClientId = clientId;
-            var authRequest = this.OAuthRequest = this.CreateOAuthRequest(user);
-            var authURI = this.OAuthURI = api.Authorization.GetOAuthURI(authRequest);
 
-            var form = this.Form = new Form();
-            var browser = this.Browser = new WebBrowser();
-            browser.Navigated += this.OnBrowserNavigated;
-            form.Controls.Add(browser);
-            form.SizeChanged += this.OnFormResize;
-            browser.Navigate(authURI);
-
-            this.UpdateBrowserSize();
-
-            Application.Run(form);
-
-            var responseURI = this.ResponseURI;
-
-            if (responseURI != null)
+            using (var authHandler = new TwitchAuthHandler(user))
             {
-                var oAuth = this.OAuthAuthorization = api.Authorization.GetOAuthAuthorization(responseURI, authRequest);
-                api.AccessToken = oAuth.AccessToken;
+                var authRequest = this.OAuthRequest = this.CreateOAuthRequest(user);
+                var authURI = api.Authorization.GetOAuthURI(authRequest);
 
-                return true;
-            }
-            else
-            {
-                return false;
+                var responseURI = authHandler.Auth(authRequest, authURI);
+
+                if (responseURI != null)
+                {
+                    var oAuth = this.OAuthAuthorization = api.Authorization.GetOAuthAuthorization(responseURI, authRequest);
+                    api.AccessToken = oAuth.AccessToken;
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
             }
 
         }
@@ -84,30 +73,6 @@ namespace TwitchAPI.Test
             request.State = Guid.NewGuid().ToString().Replace("-", "");
 
             return request;
-        }
-
-        private void OnFormResize(object sender, EventArgs e)
-        {
-            this.UpdateBrowserSize();
-        }
-
-        private void UpdateBrowserSize()
-        {
-            this.Browser.Size = this.Form.ClientSize;
-        }
-
-        private void OnBrowserNavigated(object sender, WebBrowserNavigatedEventArgs e)
-        {
-            var browser = sender as WebBrowser;
-            var url = browser.Url;
-            this.User.SendMessage($"OnBrowserNavigated : {url}");
-
-            if (url.AbsoluteUri.StartsWith(OAuthRequest.RedirectURI) == true)
-            {
-                this.ResponseURI = url;
-                this.Form.Close();
-            }
-
         }
 
     }
