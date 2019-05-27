708606beb2a2f1c4bc945fdf83679a34c50192c7
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-05-27 19:41:35 +0900

    Create ObjectUtils.cs

---
+++ Test/ObjectUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPI.Test
+{
+    public static class ObjectUtils
+    {
+        public static void DisposeQuietly(IDisposable obj)
+        {
+            if (obj != null)
+            {
+                try
+                {
+                    obj.Dispose();
+                }
+                catch (Exception)
+                {
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
