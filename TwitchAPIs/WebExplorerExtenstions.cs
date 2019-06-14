25f794fb66e1bf3c9a127a1f846fa837dd6ad9f9
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-14 18:31:12 +0900

    라이브러리 업데이트

---
+++ TwitchAPIs/WebExplorerExtenstions.cs
@@ -0,0 +1,23 @@
+﻿using Giselle.Commons.Web;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPIs
+{
+    public static class WebExplorerExtenstions
+    {
+        public static JToken ReadAsJSON(this SessionResponse session)
+        {
+            var content = session.ReadAsString();
+            var jToken = JObject.Parse(content);
+
+            return jToken;
+        }
+
+    }
+
+}
