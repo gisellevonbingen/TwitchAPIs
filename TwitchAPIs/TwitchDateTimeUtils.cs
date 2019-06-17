6375cc49958291f091c9f41bdf709e4b12c2a936
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-17 14:41:23 +0900

    트위치 DateTime 유틸 추가

---
+++ TwitchAPIs/TwitchDateTimeUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPIs
+{
+    public static class TwitchDateTimeUtils
+    {
+        public static string Format { get; } = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+        public static DateTime Parse(string s)
+        {
+            return DateTime.ParseExact(s, Format, null);
+        }
+
+        public static string ToString(DateTime dateTime)
+        {
+            return dateTime.ToString(Format);
+        }
+
+    }
+
+}
