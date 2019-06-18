65c19816f0a59d2f5c6369372502f70e0a7827bb
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-18 09:58:14 +0900

    V5 Users Get User By Id 구현

---
+++ TwitchAPIs.Test/V5/TestUsersGetUserById.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPIs.Test.V5
+{
+    [TwitchAPITest("V5", "Users")]
+    public class TestUsersGetUserByID : TestAbstract
+    {
+        public TestUsersGetUserByID()
+        {
+
+        }
+
+        public override void Run(TestMain main)
+        {
+            var user = main.User;
+            var handler = main.TwitchAPIHandler;
+
+            var userId = user.ReadInput("Enter User Id");
+            var tuser = handler.API.V5.Users.GetUserByID(userId);
+
+            main.PrintReflection(user, "TwitchUser", tuser);
+        }
+
+    }
+
+}
