0aac76b21983ea9d7332445e69799f89a6d025ca
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-18 16:30:27 +0900

    Create EnumUtils.cs

---
+++ TwitchAPIs.Test/EnumUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPIs.Test
+{
+    public static class EnumUtils
+    {
+        public static T[] GetValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)) as T[];
+        }
+
+        public static T?[] GetNullableValues<T>() where T : struct, Enum
+        {
+            return GetValues<T>().Select(v => new T?(v)).ToArray();
+        }
+
+    }
+
+}
