03302e4ed4dc0c4852e13c2ca4414a4200098972
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-19 14:30:39 +0900

    Update TestUsersGetUserFollows.cs

--- TwitchAPIs.Test/V5/TestUsersGetUserFollows.cs
+++ TwitchAPIs.Test/V5/TestUsersGetUserFollows.cs
@@ -26,7 +26,7 @@ namespace TwitchAPIs.Test.V5
             var limit = NumberUtils.ToIntNullable(user.ReadInput("Enter Limit as int"));
             var offset = NumberUtils.ToIntNullable(user.ReadInput("Enter Offset as int"));
             var direction = user.QueryInput("Enter Direction", EnumUtils.GetNullableValues<SortDirection>(), null, true).Value;
-            var sortby = user.QueryInput("Enter Sortby", EnumUtils.GetNullableValues<FollowSortMode>(), null).Value;
+            var sortby = user.QueryInput("Enter Sortby", EnumUtils.GetNullableValues<FollowSortMode>(), null, true).Value;
             var userFollows = handler.API.V5.Users.GetUserFollows(userId, limit, offset, direction, sortby);
 
             main.PrintReflection(user, "UserFollows", userFollows);
