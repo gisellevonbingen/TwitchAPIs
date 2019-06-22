e23ec5f6a5678bd6a72a3ef539f7a78fdc7c36cd
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-22 20:08:30 +0900

    New Subscriptions 추가

---
+++ TwitchAPIs/New/TwitchAPISubscriptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPIs.New
+{
+    public class TwitchAPISubscriptions : TwitchAPIPart
+    {
+        public TwitchAPISubscriptions(TwitchAPI parent) : base(parent)
+        {
+
+        }
+
+    }
+
+}
