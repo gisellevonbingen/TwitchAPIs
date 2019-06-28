ea667d143e493a6b58ce6c378c2bfcb048e67dee
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-28 14:11:25 +0900

    Other CbenniAPI APIGetBadges 추가

---
+++ TwitchAPIs.Test/Other/TestCbenniAPIGetBadges.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchAPIs.Test.Other
+{
+    [TwitchAPITest("Other", "CbenniAPI")]
+    public class TestCbenniAPIGetBadges : TestAbstract
+    {
+        public TestCbenniAPIGetBadges()
+        {
+
+        }
+
+        public override void Run(TestMain main)
+        {
+            var user = main.User;
+            var handler = main.TwitchAPIHandler;
+
+            var channelName = user.ReadInput("Enter Channel Name").AsString;
+            var set = handler.API.Other.CbenniAPI.GetBadges(channelName);
+
+            user.SendMessageAsReflection("Badges", set);
+        }
+
+    }
+
+}
